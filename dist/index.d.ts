b24fa5c4d2496934417f4b87f45816b149c2343a
Joao Araujo <joao.araujo@deutschebahn.com>
2018-03-11 22:04:59 +0100

    added: directory 'dist' to the git repository. updated: output dir for tests and npm scripts

---
+++ dist/index.d.ts
@@ -0,0 +1,1 @@
+export { deepCopyObject } from "./tsutil";
---
+++ dist/index.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var tsutil_1 = require("./tsutil");
+exports.deepCopyObject = tsutil_1.deepCopyObject;
+//# sourceMappingURL=index.js.map
