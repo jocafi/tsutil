ae596c20142e91d131a0cc229d2db914962331ca
Joao Araujo <joao.araujo@deutschebahn.com>
2018-03-15 01:00:12 +0100

    added lib directory

---
+++ lib/index.d.ts
@@ -0,0 +1,4 @@
+export { deepCopyObject } from "./deep-copy-object";
+export { readFile } from "./file-manager";
+export { writeFile } from "./file-manager";
+export { existFile } from "./file-manager";
